2eba9bec48a12c20ab080f50ad941294b05cdcf2
Tuna <tuna@sodamelon.com>
2024-09-04 11:33:53 +0100

    Try to add test page

--- blog/_modidx.py
+++ blog/_modidx.py
@@ -5,4 +5,4 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://infinitelylearn.github.io',
                 'git_url': 'https://github.com/infinitelylearn/blog',
                 'lib_path': 'blog'},
-  'syms': {'blog.core': {'blog.core.foo': ('core.html#foo', 'blog/core.py')}}}
+  'syms': {'blog.core': {'blog.core.foo': ('test.html#foo', 'blog/core.py')}}}
